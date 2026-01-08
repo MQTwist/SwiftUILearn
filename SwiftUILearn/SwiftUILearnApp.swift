8ab461cb65d92de92fdf023eb0aaaf283bb0e39a
maqi <mq943691300@163.com>
2026-01-08 18:04:12 +0800

    Initial Commit

---
+++ SwiftUILearn/SwiftUILearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearnApp.swift
+//  SwiftUILearn
+//
+//  Created by 马齐 on 2026/1/8.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
